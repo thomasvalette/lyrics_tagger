2babde3348e84b29720cfae10b2822022baf8c80
Thomas Valette <thomas.valette@protonmail.com>
2022-10-10 20:05:20 +0200

    :art: added fancier terminal prompt

--- genius.py
+++ genius.py
@@ -10,21 +10,29 @@ from bs4 import BeautifulSoup
 NB_TRIES = 3
 
 
-async def get_song_url(session, track):
+async def get_song_url(context, track):
+    session = context["session"]
+    progress = context["progress"]
+    fetch_urls = context["fetch_urls"]
     url = f"https://genius.com/api/search/multi?per_page=5&q={track.artist} {track.title}"
     async with session.get(url) as resp:
         song_info = await resp.json()
         try:
             song_url = song_info["response"]["sections"][1]["hits"][0]["result"]["path"]
+            progress.update(fetch_urls, advance=1)
             return (track, f"https://genius.com{song_url}")
         except Exception:
+            progress.update(fetch_urls, advance=1)
             return (track, None)
 
 
-async def get_song_lyrics(session, url, track, tries=0):
+async def get_song_lyrics(context, url, track, tries=0):
     """Return lyrics for the provided url
     Parsing is done with BS4 library
     """
+    session = context["session"]
+    progress = context["progress"]
+    fetch_lyrics = context["fetch_lyrics"]
     async with session.get(url) as resp:
         result = await resp.text()
         soup = BeautifulSoup(result, "html.parser")
@@ -32,6 +40,7 @@ async def get_song_lyrics(session, url, track, tries=0):
             "div", {"class": lambda L: L and L.startswith(
                 "LyricsPlaceholder")}
         ):
+            progress.update(fetch_lyrics, advance=1)
             return (track, "Instrumental")
         section = soup.find(
             "div", {"class": lambda L: L and L.startswith(
@@ -39,6 +48,7 @@ async def get_song_lyrics(session, url, track, tries=0):
         )
         # sometimes genius doesn't load properly lyrics
         try:
+            progress.update(fetch_lyrics, advance=1)
             lyrics = section.get_text("\n")
             return (track, lyrics)
         except Exception:
@@ -49,4 +59,4 @@ async def get_song_lyrics(session, url, track, tries=0):
             await asyncio.sleep(random.random()+1)
             # retry until the lyrics is retreived
             tries += 1
-            lyrics = await get_song_lyrics(session, url, tries)
+            lyrics = await get_song_lyrics(session, url, progress, tries)
--- tracks.py
+++ tracks.py
@@ -3,21 +3,35 @@ import asyncio
 import os
 import sys
 from rich import print
+from rich.progress import Progress
+from rich.prompt import Confirm
+from rich.table import Table
 from mediafile import MediaFile, FileTypeError
 from genius import get_song_lyrics, get_song_url
 
 
 def get_tracks_from_directory(path):
+
+    print(f"\n Selected path : [yellow][bold]{path}[/bold]")
+    total_nb_files = 0
+    for _, _, files in os.walk(path):
+        total_nb_files += len(files)
+
     tracks = []
-    for root, _, files in os.walk(path):
-        for file in files:
-            file_path = os.path.join(root, file)
-            try:
-                track = MediaFile(file_path)
-                tracks.append(track)
-            except FileTypeError:
-                pass
-    return tracks
+    with Progress() as progress:
+        file_crawl = progress.add_task(
+            "[yellow] Scanning local files ...            ", total=total_nb_files)
+        for root, _, files in os.walk(path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                try:
+                    track = MediaFile(file_path)
+                    tracks.append(track)
+                except FileTypeError:
+                    pass
+                finally:
+                    progress.update(file_crawl, advance=1)
+        return tracks
 
 
 async def tag_track(session, track):
@@ -26,44 +40,80 @@ async def tag_track(session, track):
     lyrics = await get_song_lyrics(session, url)
     track.lyrics = lyrics
     track.save()
-    print(f"{track.title} saved!")
 
 
 async def tag_all_tracks(tracks):
-    track_not_found = []
+    url_not_found = []
     lyric_not_found = []
-    nb_tracks = 0
-    async with aiohttp.ClientSession() as session:
-        tasks_url = []
-        # retrieve all lyrics
-        for track in tracks:
-            tasks_url.append(asyncio.ensure_future(
-                get_song_url(session, track)))
-        urls = await asyncio.gather(*tasks_url)
+    tagged_tracks = 0
+    context = {}
 
-        tasks_lyric = []
+    if Confirm.ask(f" {len(tracks)} tracks were found, continue?"):
 
-        # retrieve all lyrics
-        for url in urls:
-            if url[1] is None:
-                track_not_found.append(f"{url[0].artist} - {url[0].title}")
-            else:
-                tasks_lyric.append(asyncio.ensure_future(
-                    get_song_lyrics(session, url[1], url[0])))
-        lyrics_list = await asyncio.gather(*tasks_lyric)
+        # creatikng progress bars for fancier display
+        with Progress() as progress:
 
-        # tag all tracks
-        for lyrics in lyrics_list:
-            if lyrics[1] is None:
-                lyric_not_found.append(
-                    f"{lyrics[0].artist} - {lyrics[0].title}")
-            else:
-                track = lyrics[0]
-                track.lyrics = lyrics[1]
-                track.save()
-                nb_tracks += 1
+            fetch_urls = progress.add_task(
+                "[blue] Fetching Genius urls ...   ", total=len(tracks))
+            fetch_lyrics = progress.add_task(
+                "[cyan] Fetching tracks lyrics & tagging ...", total=len(tracks))
 
-        return nb_tracks, track_not_found, lyric_not_found
+            context["progress"] = progress
+            context["fetch_urls"] = fetch_urls
+            context["fetch_lyrics"] = fetch_lyrics
+
+            async with aiohttp.ClientSession() as session:
+                context["session"] = session
+                tasks_url = []
+                # retrieve all lyrics
+                for track in tracks:
+                    tasks_url.append(asyncio.ensure_future(
+                        get_song_url(context, track)))
+                urls = await asyncio.gather(*tasks_url)
+                tasks_lyric = []
+
+                # retrieve all lyrics
+                for url in urls:
+                    if url[1] is None:
+                        url_not_found.append((url[0].artist, url[0].title))
+                        progress.update(fetch_lyrics, advance=1)
+                    else:
+                        tasks_lyric.append(asyncio.ensure_future(
+                            get_song_lyrics(context, url[1], url[0])))
+                lyrics_list = await asyncio.gather(*tasks_lyric)
+
+                # tag all tracks
+                for lyrics in lyrics_list:
+                    if lyrics[1] is None:
+                        lyric_not_found.append(
+                            lyrics[0].artist, lyrics[0].title)
+                    else:
+                        track = lyrics[0]
+                        track.lyrics = lyrics[1]
+                        track.save()
+                        tagged_tracks += 1
+
+        print("\n End Report: ")
+        print(" ━━━━━━━━━━━")
+        print(f"[green]Tagged track : {tagged_tracks}[/green]")
+        print(f"[red]Url not found : {len(url_not_found)}[/red]")
+        print(f"[red]Lyrics not found : {len(lyric_not_found)}[/red]")
+
+        if Confirm.ask("\nDo you want to display error details?"):
+            table = Table()
+
+            table.add_column("Artist", style="cyan")
+            table.add_column("Title", style="magenta")
+            table.add_column("Error", justify="right")
+
+            for err in url_not_found:
+                table.add_row(err[0], err[1], "[yellow]Url Not Found[/yellow]")
+
+            for err in lyric_not_found:
+                table.add_row(err[0], err[1], "[red]Lyrics Not Found[/red]")
+
+            print(table)
+
 
 if __name__ == "__main__":
     # Only preform check if your code will run on non-windows environments.
@@ -71,8 +121,7 @@ if __name__ == "__main__":
         # Set the policy to prevent "Event loop is closed" error on Windows - https://github.com/encode/httpx/issues/914
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-    tracks = get_tracks_from_directory(r"D:\tmp")
+    tracks = get_tracks_from_directory(
+        r"D:\Thomas\Music\FLAC_old")
 
-    a, b, c = asyncio.run(tag_all_tracks(tracks))
-
-    print(a, b, c)
+    asyncio.run(tag_all_tracks(tracks))
